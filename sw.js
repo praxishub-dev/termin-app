0fcc97857ed47104c0b6443dfa7549c83fdf7ddc
praxishub-dev <privat@specht-waschkowski.de>
2026-02-25 20:53:22 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'therapie-v2';
+const BASE = '/termin-app';
+const ASSETS = [BASE + '/', BASE + '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
